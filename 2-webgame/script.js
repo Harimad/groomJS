f625eead21c12eac4d68108ddd342ad7c9078438
Harimad <leesh68481@gmail.com>
2022-02-21 19:57:54 +0900

    데이터 정의

---
+++ 2-webgame/script.js
@@ -0,0 +1,30 @@
+$(documnet).ready(function () {
+  //데이터 정의
+
+  //공의 개수
+  let circleNumber = 0;
+
+  //공의 종류 - 지름, 반지름, 색, 움직이는 속도
+  let circleTypes = {
+    'option': ['color', 'width', 'border-radius', 'speed'],
+    'small': ['black', 5, 2.5, 3000], //3000은 한 지점에서 다른 지점으로 움직일때 걸리는 millisecond임
+    'medium': ['blue', 15, 7.5, 4000],
+    'large': ['yello', 30, 15, 5000],
+  };
+
+  //e.g circleChoice 라는 변수에 small
+  //circleTypes[circleChoice][0] = 색
+  //circleTypes[circleChoice][0] = 지름
+  //circleTypes[circleChoice][0] = 반지름
+  //circleTypes[circleChoice][0] = 속도
+
+  //시간을 찍어주는 변수
+  let t = 0;
+
+  //게임 실행 여부
+  let gameOn = false;
+
+  //마우스 좌표
+  let mouseX;
+  let mouseY;
+});
