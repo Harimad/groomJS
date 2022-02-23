daa03fcac58666b44b1af4a15b529bfa4c95b77a
Harimad <leesh68481@gmail.com>
2022-02-23 17:41:37 +0900

    popup html 구현

--- 4-Chrome-Extension-Project/typedUrls.js
+++ 4-Chrome-Extension-Project/typedUrls.js
@@ -7,3 +7,23 @@ function onAnchorClick(event) {
   });
   return false; //위의 create가 실패하면 false 리턴
 }
+
+//Top 10 Visited url 배열이 주어졌을 때 각각의 url들을 typedUrls.html에 띄워주기
+function buildPopupDom(divName, data) {
+  let popDiv = document.querySelector('#' + divName);
+
+	let ul = document.createElement('ul');
+	popDiv.appendChild(ul);
+
+	for (let i = 0, ie = data.length; i < ie, ++i) {
+		let a = document.createElement('a');
+		a.href = data[i];
+		a.appendChild(document.createTextNode(data[i]));
+		a.addEventListener('click', onAnchorClick);
+
+		let li = document.createElement('li');
+		li.appendChild(a);
+		ul.appendChild(li);
+
+	}
+}
